0524584dfca8d1f69dacec890e1fec013311b949
spreston13 <160267237+spreston13@users.noreply.github.com>
2024-02-23 17:04:11 -0700

    Update cis129_lab02_shortStory.py
    I added more lines of the code to include a story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,6 @@
+# Author: Sebastian Preston
+# My first Python lab in my CIS129 course
+# Prints a simple short story about the morning.
+
 print('Hello, World!')
+print("It was dawn and the flags rose up. It was a cool crisp morning. Dogs were chasing the first things they see and hear, their alarm clock. It's time to start the day")
