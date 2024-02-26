dd536b88d7f075a51e6c80565ae26330f80237cd
spreston13 <160267237+spreston13@users.noreply.github.com>
2024-02-26 16:38:41 -0700

    Update cis129_lab03_coffeeShop.py
    Added 2 more items to my shop

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,18 +1,38 @@
+print('*' * 40)
 print('My Coffee and Muffin Shop')
-coffees = input('Number of coffees bought?' ) 
-muffins = input('Number of muffins bought?' )
-
-num_of_coffees = coffees
-num_of_muffins = muffins
-
-price_of_coffee = 5
-price_of_muffin = 4
-subtotal = price_of_coffee + price_of_muffin
-
-
+#Declare integer numOfCoffees
+print('Number of coffees bought?')
+numOfCoffees = input()
+#Declare integer numOfMuffins
+print('Number of muffins bought?')
+numOfMuffins = input()
+#Declare integer numOfTeas
+print('Number of teas bought?')
+numOfTeas = input()
+#Declare integer numOfSandwiches
+print('Number of sandwiches bought?')
+numOfSandwiches = input()
+print('*' * 40)
+#Price number of items bought by sale price
+cPrice = int(numOfCoffees) * 5
+mPrice = int(numOfMuffins) * 4
+tPrice = int(numOfTeas) * 3
+sPrice = int(numOfSandwiches) * 6
+#Display the receipt with added tax
 print('My Coffee and Muffin Shop Receipt')
-print(int(num_of_coffees), 'at $5 each:' num_of_coffees * int(price_of_coffee)
-print(int(num_of_muffins), 'at $4 each:' num_of_muffins * int(price_of_muffin)
-print('6% tax:') subtotal % float(.04)
-
-
+cBought = str(numOfCoffees)
+mBought = str(numOfMuffins)
+tBought = str(numOfTeas)
+sBought = str(numOfSandwiches)
+print(cBought + ' coffees at $5 each: $ '+ str(cPrice))
+print(mBought + ' muffins at $4 each: $ '+ str(mPrice))
+print(tBought + ' teas at $3 each: $ '+ str(tPrice))
+print(sBought + ' sandwiches at $6 each: $ '+ str(sPrice))
+#Get the subtotal 
+subTotal = cPrice + mPrice + tPrice + sPrice
+tax = subTotal * .06
+print('6% tax: $' + str(tax))
+print('─' * 30)
+finalTotal = subTotal + tax
+print('Total: $'+ str(finalTotal))
+print('*' * 40)
