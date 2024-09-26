1327e001f022bff33492a1ea152d41caff44cf09
spreston13 <160267237+spreston13@users.noreply.github.com>
2024-09-25 17:20:30 -0700

    Update cis129_lab03_coffeeShop.py
    first stab

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,1 +1,18 @@
+#This program will ask the user for the number of coffee and muffins they are purchasing. The price of a cup of coffee is $5 and the price of a muffin is $4. 
+#Calculate 6% tax on the subtotal.
 
+#Declare variables and constants needed for this program
+product_1 = "coffee"
+product_2 = "muffins"
+num_of_coffees = input(int())
+num_of_muffins = input(int())
+coffee_price = 5.00
+muffin_price = 4.00
+
+print("My Coffee and Muffin Shop")
+print("Number of coffees bought?")
+input(int())
+
+print("Number of muffins bought?")
+input(int())
+      
