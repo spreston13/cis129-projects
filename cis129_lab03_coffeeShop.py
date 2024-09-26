770ecc3ef8c3459b4ffbe626c132b49ceea4cdb6
spreston13 <160267237+spreston13@users.noreply.github.com>
2024-09-25 22:57:58 -0700

    Update cis129_lab03_coffeeShop.py

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,18 +1,27 @@
 #This program will ask the user for the number of coffee and muffins they are purchasing. The price of a cup of coffee is $5 and the price of a muffin is $4. 
 #Calculate 6% tax on the subtotal.
 
-#Declare variables and constants needed for this program
-product_1 = "coffee"
-product_2 = "muffins"
-num_of_coffees = input(int())
-num_of_muffins = input(int())
-coffee_price = 5.00
-muffin_price = 4.00
-
+#This section gathers input from the customer on how many items they purchased
+print("*"*39)
 print("My Coffee and Muffin Shop")
-print("Number of coffees bought?")
-input(int())
+print("How many coffees bought?: ")
+cBought = int(input())
+print("How many muffins bought?: ")
+mBought = int(input()) 
+print("*"*39)
 
-print("Number of muffins bought?")
-input(int())
-      
+#This section itemizes and taxes the products
+print("*"*39)
+print("My Coffee and Muffin Shop Receipt")
+cSum = cBought * 5.00 
+print(str(cBought)+" Coffees bought at $5 each:",cSum)
+mSum = mBought * 4.00
+print(str(mBought)+" Muffins bought at $4 each:",mSum)
+tax = (cSum + mSum)*.06
+print("6% tax: $ "+str(tax))
+
+#This section calculates the grand total
+print("-"*15)
+total = cSum + mSum + tax
+print("Total: "+str(total))
+print("*"*39)
