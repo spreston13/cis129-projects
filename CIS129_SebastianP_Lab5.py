b8882616be21053c777b76dcc8474d550da5a358
spreston13 <160267237+spreston13@users.noreply.github.com>
2024-10-08 08:57:44 -0700

    Update CIS129_SebastianP_Lab5.py

--- CIS129_SebastianP_Lab5.py
+++ CIS129_SebastianP_Lab5.py
@@ -5,23 +5,23 @@
 #This program takes input from the user to calculate the payout for the sum of bottles turned in
 
 totalBottles = 0
-counter = 1	
+counter = 1
+todayBottles = 0
 keepGoing = "y"
 
 # Step 2: Loop to run program again
 while counter <= 7:
   print('Enter number of bottles returned for day #', counter, ':')
-  todayBottles = int(input())
-  totalBottles += todayBottles
   counter = counter + 1
+  input() 
+  totalBottles += todayBottles
 
 # code to set value of variable totalPayout
 PAYOUT_PER_BOTTLE = .10
 totalPayout = 0
-totalPayout = totalBottles * PAYOUT_PER_BOTTLE	
+totalPayout = totalBottles * PAYOUT_PER_BOTTLE
 
-# code to print the number of total bottles and total payout
-print('The total number of bottles returned was'), str(totalBottles)
+print('The total number of bottles returned was'), sum
 print('The total payout for the bottles is $'), totalPayout
 		
 print("Do you want to enter another week’s worth of data?")
