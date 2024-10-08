1cba0ea00f3482971d68b00fc80293f8d4825c0d
spreston13 <160267237+spreston13@users.noreply.github.com>
2024-10-08 15:13:53 -0700

    Update CIS129_SebastianP_Lab5.py
    Correct version that works

--- CIS129_SebastianP_Lab5.py
+++ CIS129_SebastianP_Lab5.py
@@ -11,18 +11,29 @@ keepGoing = "y"
 
 # Step 2: Loop to run program again
 while counter <= 7:
-  print('Enter number of bottles returned for day #', counter, ':')
-  counter = counter + 1
-  input() 
+  # ask how many bottles for the day
+  # print statement for formatting leave as is
+  print('Enter number of bottles returned for day', counter, ':')
+  # store blank input in the todayBottles variable. remember you need to turn it into a integer. 
+  #ok how do we add all the bottles now?? totalBottles += todayBottles
+  todayBottles = int(input())
   totalBottles += todayBottles
+  counter = counter + 1
+# i dont understand how it works with counter but not the totalBottles okay. this is the long way
+# totalBottles = totalBottles + todayBottles
 
 # code to set value of variable totalPayout
 PAYOUT_PER_BOTTLE = .10
 totalPayout = 0
-totalPayout = totalBottles * PAYOUT_PER_BOTTLE
+# I am trying to multiply an integer by a decimal and that is a no no for any programming language
+# they need to be of the same type
+# ok hold up! why isnt it adding my other part tho? the totalBottles part, they are all the same type??
+totalPayout = float(totalBottles) * PAYOUT_PER_BOTTLE
 
-print('The total number of bottles returned was'), totalBottles
-print('The total payout for the bottles is $'), totalPayout
-		
+print('The total number of bottles returned was', totalBottles) # I didn't include totalBottles inside the print statement
+print('The total payout for the bottles is $', totalPayout)
+# NIIIICEEEEEE
 print("Do you want to enter another week’s worth of data?")
 print("Enter y or n")
+# it helps to lay out the instructions
+#i want it to have the days for each input, see how it works now???
